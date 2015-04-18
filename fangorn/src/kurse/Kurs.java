cee604ce48998b476ba8a764413430040cf8e364
RalfMartini <ralf.martini@freenet.de>
2015-04-18 14:17:54 +0200

    Kurs added

---
+++ fangorn/src/kurse/Kurs.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package kurse;
+
+//import java.util.Calendar;
+
+/**
+ * @author Ralf
+ *
+ */
+public class Kurs {
+	
+	private String sName;
+	private String sQuelle;
+	private String sKuerzel;
+	private String sBoerse;
+	private String sGueltigBis;
+	private String sGueltigVon;
+	private String sDatum;
+	private String sEroeffnung;
+	private String sMax;
+	private String sTief;
+	private String sSchluss;
+	private String sVolumen;
+	
+	public Kurs(){
+	}
+}
